bc2430e03262f68c3b5f43f613b1a87477df2e86
ITAndrii <130089458+ITAndrii@users.noreply.github.com>
2023-11-19 21:39:09 +0200

    dz_finish

---
+++ js/task-4.js
@@ -0,0 +1,31 @@
+function getShippingCost(country) {
+  switch (country) {
+    case 'China':
+      deliveryMessage = `Shipping to ${country} will cost 100 credits`;
+      break;
+
+    case 'Chile':
+      deliveryMessage = `Shipping to ${country} will cost 250 credits`;
+      break;
+
+    case 'Australia':
+      deliveryMessage = `Shipping to ${country} will cost 170 credits`;
+      break;
+
+    case 'Jamaica':
+      deliveryMessage = `Shipping to ${country} will cost 120 credits`;
+      break;
+
+    default:
+      deliveryMessage = 'Sorry, there is no delivery to your country';
+      break;
+  }
+  return deliveryMessage;
+}
+
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
